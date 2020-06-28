4f243a3b4cd5f92389428344c567a749a80131ed
Sean <Stp29@users.noreply.github.com>
2020-06-28 08:44:57 -0500

    Update relative-animation.js
    Fixed bug of animation not starting if the tenths place in Math.randdom() was 0
    Ex. 0.0932492

--- learning/advanced-positioning/relative-animation.js
+++ learning/advanced-positioning/relative-animation.js
@@ -2,33 +2,33 @@
   var ele = document.querySelector('.item-relative');
 
   var left = 0;
-  var xSpeed = parseInt(Math.random() * 10, 10);
+  var xSpeed = parseInt(Math.random() * 9 + 1, 10);
   var xDisplacement = xSpeed;
   
   function MoveFrameSideways() {
     left += xDisplacement;
     ele.style.left = left + 'px';
     if (left >= 300) {
-      xSpeed = parseInt(Math.random() * 10, 10);
+      xSpeed = parseInt(Math.random() * 9 + 1, 10);
       xDisplacement = -1 * xSpeed;
     } else if (left <= -300) {
-      xSpeed = parseInt(Math.random() * 10, 10);
+      xSpeed = parseInt(Math.random() * 9 + 1, 10);
       xDisplacement = xSpeed;
     }
   }
 
   var down = 0;
-  var ySpeed = parseInt(Math.random() * 5, 10);
+  var ySpeed = parseInt(Math.random() * 4 + 1, 10);
   var yDisplacement = ySpeed;
 
   function MoveFrameUpAndDown() {
     down += yDisplacement;
     ele.style.top = down + 'px';
     if (down >= 300) {
-      ySpeed = parseInt(Math.random() * 5, 10);
+      ySpeed = parseInt(Math.random() * 4 + 1, 10);
       yDisplacement = -1 * ySpeed;
     } else if (down <= -100) {
-      ySpeed = parseInt(Math.random() * 5, 10);
+      ySpeed = parseInt(Math.random() * 4 + 1, 10);
       yDisplacement = ySpeed;
     }
   }
