c432a6ad7e6a127f1a6949e93b1c8639321269c6
Sean <Stp29@users.noreply.github.com>
2020-06-28 08:34:21 -0500

    Various Updates

--- learning/advanced-positioning/relative-animation.js
+++ learning/advanced-positioning/relative-animation.js
@@ -1,16 +1,38 @@
 (function() {
+  var ele = document.querySelector('.item-relative');
+
   var left = 0;
+  var xSpeed = parseInt(Math.random() * 10, 10);
+  var xDisplacement = xSpeed;
   
-  function frame() {
-    var ele = document.querySelector('.item-relative');
-    left += 2;
+  function MoveFrameSideways() {
+    left += xDisplacement;
     ele.style.left = left + 'px';
     if (left >= 300) {
-      clearInterval(id);
-      left = 0;
-      id = setInterval(frame, 12);
+      xSpeed = parseInt(Math.random() * 10, 10);
+      xDisplacement = -1 * xSpeed;
+    } else if (left <= -300) {
+      xSpeed = parseInt(Math.random() * 10, 10);
+      xDisplacement = xSpeed;
     }
   }
-  
-  var id = setInterval(frame, 12);
+
+  var down = 0;
+  var ySpeed = parseInt(Math.random() * 5, 10);
+  var yDisplacement = ySpeed;
+
+  function MoveFrameUpAndDown() {
+    down += yDisplacement;
+    ele.style.top = down + 'px';
+    if (down >= 300) {
+      ySpeed = parseInt(Math.random() * 5, 10);
+      yDisplacement = -1 * ySpeed;
+    } else if (down <= -100) {
+      ySpeed = parseInt(Math.random() * 5, 10);
+      yDisplacement = ySpeed;
+    }
+  }
+
+  var Id1 = setInterval(MoveFrameSideways, 12);
+  var Id2 = setInterval(MoveFrameUpAndDown, 12);
 })();
--- learning/flexbox/date-loader.js
+++ learning/flexbox/date-loader.js
@@ -1,5 +1,5 @@
 (function() {
-  var ele = document.querySelector('.menu .date');
+  var ele = document.querySelector('.menu > .date');
   var today = new Date();
   ele.innerHTML = today.toDateString();
 })();
